77bc8112776b9cedd2415376f0ee501a894bbcaa
turneps403 <turneps403@mail.ru>
2022-06-20 15:26:41 +0300

    - aoption

--- dls/main.go
+++ dls/main.go
@@ -17,20 +17,31 @@ var (
 
 func main() {
 	initDir := "./"
+	opts := []string{}
 	if len(os.Args) > 1 {
 		initDir = os.Args[len(os.Args)-1]
+		opts = os.Args[1 : len(os.Args)-1]
 	}
 	if initDir == "-h" || initDir == "-help" || initDir == "h" || initDir == "help" {
-		fmt.Println(`shows list of directories and subdirectories by path and max deep 100`)
+		fmt.Println(`-D == dont show directories; -d show only directories`)
 		os.Exit(0)
 	}
 	if initDir[0] != '/' {
 		initDir = filepath.Join(basepath, initDir)
 	}
-	recurSearch(initDir, MAX_DEEP)
+	recurSearch(initDir, opts, MAX_DEEP)
 }
 
-func recurSearch(initDir string, deep int) {
+func inOpts(o string, opts []string) bool {
+	for _, v := range opts {
+		if v == o {
+			return true
+		}
+	}
+	return false
+}
+
+func recurSearch(initDir string, opts []string, deep int) {
 	if deep < 1 {
 		return
 	}
@@ -42,8 +53,14 @@ func recurSearch(initDir string, deep int) {
 	for _, f := range files {
 		if f.IsDir() {
 			fullDirPath := filepath.Join(initDir, f.Name())
+			if !inOpts("-D", opts) {
+				fmt.Println(fullDirPath)
+			}
+			recurSearch(fullDirPath, opts, deep-1)
+		}
+		if !f.IsDir() && !inOpts("-d", opts) {
+			fullDirPath := filepath.Join(initDir, f.Name())
 			fmt.Println(fullDirPath)
-			recurSearch(fullDirPath, deep-1)
 		}
 	}
 }
